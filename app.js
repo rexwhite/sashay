44f6888201cf5aa2526d7ae8f41c9ddabc68e073
Rex White <rexwhite@gmail.com>
2022-07-06 12:04:33 -0400

    Playing with rendering pug templates, mostly

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT | 8080;
 
+app.locals.port = port;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('./public'));
+
+const data = {
+    apis: [
+        'playbook-dispatcher',
+        'playbook-dispatcher internal',
+        'remediations',
+        'sources'
+    ]
+};
+
 app.get('/', (req, res) => {
-    res.render('hello.jade');
+    res.render('hello', data);
 });
 
 // get list of swagger specs
