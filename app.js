53751b8cdf47652168c5b04a01f15434ca69f153
Rex White <rexwhite@redhat.com>
2022-07-06 21:11:05 -0400

    Added code to render swagger

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 'use strict';
 
+const fs = require('fs').promises;
+const path = require('path');
+const _ = require('lodash');
+const yaml = require('yamljs');
 const express = require('express');
-const app = express();
+const swaggerUI = require('swagger-ui-express');
+
+const SCHEMA_DIR = 'schemas';
 const port = process.env.PORT | 8080;
+const app = express();
 
 app.locals.port = port;
 
@@ -11,23 +18,33 @@ app.set('views', './views');
 
 app.use(express.static('./public'));
 
-const data = {
-    apis: [
-        'playbook-dispatcher',
-        'playbook-dispatcher internal',
-        'remediations',
-        'sources'
-    ]
-};
-
 app.get('/', (req, res) => {
-    res.render('hello', data);
+    // get list of schema files
+    return fs.readdir('./schemas')
+    .then((files) => {
+        const list = _.filter(files,  (filename) => {
+            const file = filename.toLowerCase();
+            return file.endsWith('.yml') || file.endsWith('.yaml');
+        });
+
+        return res.render('hello', {apis: list});
+    });
 });
 
-// get list of swagger specs
-// generate home page that lists links to loaded specs
-// serve specs at link
+const loadSchema = (req, res, next) => {
+    // res.send(`Loading file: ${req.params.filename}...`);
+    return yaml.load(path.join(SCHEMA_DIR, req.params.filename), (result) => {
+        req.swaggerDoc = result;
+        console.log(`swaggerUI.serve: ${JSON.stringify(swaggerUI.serve)}`);
+        next();
+    });
+};
 
+// serve specs at link
+app.use(`/apidoc/:filename`, loadSchema, swaggerUI.serve, swaggerUI.setup());
+
+
+// start the server
 app.listen(port,  () => {
     console.log(`listening on port: ${port}.`);
 });
