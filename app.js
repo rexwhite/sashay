8f4d8eb9f5c2c28c19c09f4f67a42bf573351ddf
Rex White <rexwhite@redhat.com>
2024-05-10 16:02:25 -0400

    Simplified logic, now serves selected api spec with no preloading.

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const port = process.env.PORT | 8080;
 const app = express();
 
 app.locals.port = port;
-app.locals.schemaList = {};
 
 function loadSchemaList(directory=`${SCHEMA_DIR}`) {
     const list = [];
@@ -39,61 +38,22 @@ function loadSchemaList(directory=`${SCHEMA_DIR}`) {
     return list;
 };
 
+// load schema list
+const schemaList = loadSchemaList();
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-
-app.use((req, res, next) => {
-    console.log(`called ${req.url}`);
-    next();
-});
-
-app.use('/doc/schemas/', express.static('./schemas'));
+app.use('/apidoc/schemas/', express.static('./schemas'));
 
 app.get('/', (req, res) => {
-        return res.set('Cache-Control', 'no-store').render('list', {apis: app.locals.schemaList});
+        return res.set('Cache-Control', 'no-store').render('list', {apis: schemaList});
 });
 
-const loadSchema = (file) => {
-    if (file.endsWith('.yaml') || file.endsWith('*.yml')) {
-        return new Promise((resolve, reject) => {
-            try {
-                yaml.load(path.join(SCHEMA_DIR, file), result => resolve(result));
-            }
-            catch (err) {
-                reject(err);
-            }
-        });
-    }
-
-    if (file.endsWith('.json')) {
-        return fs.readFile(path.join(SCHEMA_DIR, file), 'utf8')
-        .then(result => JSON.parse(result));
-    }
-};
-
-const serveSchema = (req, res, next) => {
-    req.swaggerDoc = loadSchema('./dispathcer/private.openapi.yaml');
-    next();
-};
-
-const temp_1 = (req, res, next) => {
-    next();
-};
-
-const temp_2 = (req, res, next) => {
-    const options = {swaggerOptions: {url: '.' + req.path}};
+app.use('/apidoc', swaggerUI.serve, (req, res, next) => {
+    const options = {swaggerOptions: {url: `.${req.query.spec}`}}
     return swaggerUI.setup(null, options)(req, res, next);
-};
-
-// serve specs at link
-app.use('/apidoc', temp_1, serveSchema, swaggerUI.serveFiles(), swaggerUI.setup());
-
-const doc = yaml.load('./schemas/dispatcher/private.openapi.yaml');
-app.use('/doc', swaggerUI.serve, swaggerUI.setup(null, {swaggerOptions: {url: './schemas/dispatcher/private.openapi.yaml'}}));
-
-// load schemas
-app.locals.schemaList = loadSchemaList()
+});
 
 // start the server
 app.listen(port,  () => {
